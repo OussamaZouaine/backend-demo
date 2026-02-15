7865554ffad2e7694d2fd4bf509608c7d6d23a8a
Oussama Zouaine <oussamazouaine@gmail.com>
2026-02-15 14:28:07 +0000

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Elysia } from "elysia";
+import { openapi } from "@elysiajs/openapi";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().use(openapi());
+
+app.get("/", () => Response.json({ message: "Hello Elysia" }));
+
+app.listen(3010)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
